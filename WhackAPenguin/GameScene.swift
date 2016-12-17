56b598fa56fae12d8b8999be8e5eead1ab4ea2ef
noahpatterson <n@noahpatterson.me>
2016-12-17 16:34:25 -0700

    add background and score label

--- WhackAPenguin/GameScene.swift
+++ WhackAPenguin/GameScene.swift
@@ -11,9 +11,29 @@ import GameplayKit
 
 class GameScene: SKScene {
 
+    var gameScoreLabel: SKLabelNode!
+    var score: Int = 0 {
+        didSet {
+            gameScoreLabel.text = "Score: \(score)"
+        }
+    }
+    
     
     override func didMove(to view: SKView) {
+        // add background
+        let background = SKSpriteNode(imageNamed: "whackBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
         
+        //add score label
+        gameScoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        gameScoreLabel.text = "Score: 0"
+        gameScoreLabel.position = CGPoint(x: 8, y: 8)
+        gameScoreLabel.horizontalAlignmentMode = .left
+        gameScoreLabel.fontSize = 48
+        addChild(gameScoreLabel)
     }
     
     
