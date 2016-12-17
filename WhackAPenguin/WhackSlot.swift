f52858495389cfd9e646cfb8014c68224fb4aad4
noahpatterson <n@noahpatterson.me>
2016-12-17 16:35:04 -0700

    add custom SKNode for WhackSlot

---
+++ WhackAPenguin/WhackSlot.swift
@@ -0,0 +1,14 @@
+//
+//  WhackSlot.swift
+//  WhackAPenguin
+//
+//  Created by Noah Patterson on 12/17/16.
+//  Copyright © 2016 noahpatterson. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class WhackSlot: SKNode {
+
+}
