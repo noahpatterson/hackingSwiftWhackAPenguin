5026c4f6acc1185250957541f464bf98ec1c7f0d
noahpatterson <n@noahpatterson.me>
2016-12-17 16:58:03 -0700

    add the crop nodes and penguin, then hide the penguins within the node

--- WhackAPenguin/WhackSlot.swift
+++ WhackAPenguin/WhackSlot.swift
@@ -10,11 +10,26 @@ import UIKit
 import SpriteKit
 
 class WhackSlot: SKNode {
+    
+    var charNode: SKSpriteNode!
 
     func configure(at position: CGPoint) {
         self.position = position
         let sprite = SKSpriteNode(imageNamed: "whackHole")
         
         addChild(sprite)
+        
+        //add penguin image and crop mask -- mask hide the penguin when it moves
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
     }
 }
