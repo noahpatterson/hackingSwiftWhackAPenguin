2cb272d9081bf3b857acdf799fc8c8765cc1d466
noahpatterson <n@noahpatterson.me>
2016-12-18 06:43:09 -0700

    allow penguins to be created and show at random. Hide penguins after a period of time

--- WhackAPenguin/GameScene.swift
+++ WhackAPenguin/GameScene.swift
@@ -13,6 +13,7 @@ class GameScene: SKScene {
 
     var gameScoreLabel: SKLabelNode!
     var slots = [WhackSlot]()
+    var popupTime = 0.85
     var score: Int = 0 {
         didSet {
             gameScoreLabel.text = "Score: \(score)"
@@ -41,6 +42,8 @@ class GameScene: SKScene {
         for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 320)) }
         for i in 0..<5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230)) }
         for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 140)) }
+        
+        createEnemy()
     }
     
     
@@ -54,6 +57,41 @@ class GameScene: SKScene {
         slot.configure(at: position)
         addChild(slot)
         slots.append(slot)
+    }
+    
+    func createEnemy() -> Void {
+        //decrease popup time each time to make the game faster
+        popupTime *= 0.991
+        
+        //shuffle slots
+        slots = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: slots) as! [WhackSlot]
+        
+        //make slot show itself
+        slots[0].show(hideTime: popupTime)
+        
+        //see if more slots should be shown
+        if RandomInt(min: 0, max: 12) > 4 {
+            slots[1].show(hideTime: popupTime)
+        }
+        if RandomInt(min: 0, max: 12) > 8 {
+            slots[2].show(hideTime: popupTime)
+        }
+        if RandomInt(min: 0, max: 12) > 10 {
+            slots[3].show(hideTime: popupTime)
+        }
+        if RandomInt(min: 0, max: 12) > 11 {
+            slots[4].show(hideTime: popupTime)
+        }
+        
+        //call this function again after a delay
+        let minDelay = popupTime / 2.0
+        let maxDelay = popupTime * 2
+        let delay    = RandomDouble(min: minDelay, max: maxDelay)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+            [unowned self] in
+            self.createEnemy()
+        }
     }
     
 }
--- WhackAPenguin/WhackSlot.swift
+++ WhackAPenguin/WhackSlot.swift
@@ -12,6 +12,8 @@ import SpriteKit
 class WhackSlot: SKNode {
     
     var charNode: SKSpriteNode!
+    var isVisible = false
+    var isHit     = false
 
     func configure(at position: CGPoint) {
         self.position = position
@@ -31,5 +33,33 @@ class WhackSlot: SKNode {
         cropNode.addChild(charNode)
         
         addChild(cropNode)
+    }
+    
+    func show(hideTime: Double) {
+        if isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        
+        isVisible = true
+        isHit     = false
+        
+        if RandomInt(min: 0, max: 2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name    = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name    = "charEnemy"
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) {
+            [unowned self] in
+            self.hide()
+        }
+    }
+    
+    func hide() {
+        if !isVisible { return }
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        isVisible = false
     }
 }
